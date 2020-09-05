371a6cd9798f2d4f9f915bb97998b09ee2725365
RyanGeoffray <70250272+RyanGeoffray@users.noreply.github.com>
2020-09-05 15:58:07 -0700

    formatting script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.addEventListener('load', ()=> {
+
+
+window.addEventListener('load', () => {
   let long;
   let lat;
   let temperatureDescription = document.querySelector('.temperature-description');
@@ -6,33 +8,31 @@ window.addEventListener('load', ()=> {
   let locationTimezone = document.querySelector('.location-timezone');
   let icon = document.querySelector('.icon');
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position =>{
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
       const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=dd088661d976220ba9d4d12cf3d1d70c`;
-      
+
       //const pic = `http://openweathermap.org/img/wn/${icon}@2x.png`;
       fetch(api)
-      //fetch(pic)
-      .then(response =>{
-        return response.json();
-      })
-      .then(data =>{
-        console.log(data);
-        const{description, weatherIcon} = data.weather[0];
-        const{temp_max} = data.main;
-        const{name} = data;
-        //set DOM elements from API
-        temperatureDegree.textContent = temp_max;
-        temperatureDescription.textContent = description;
-        locationTimezone.textContent = name;
-        icon.backgroundImage = 
+        //fetch(pic)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          const { description, weatherIcon } = data.weather[0];
+          const { temp_max } = data.main;
+          const { name } = data;
+          //set DOM elements from API
+          temperatureDegree.textContent = temp_max;
+          temperatureDescription.textContent = description;
+          locationTimezone.textContent = name;
+          //icon.backgroundImage = 
       })
     });
     //const api2 = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-  //function setIcons (icon, iconID){
+    //function setIcons (icon, iconID){
     //const skycons = new skycons({color: 'white'});
     //const currentIcon = weatherIcon.replace(/-/g, "_")
   }
