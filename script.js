3497d69b9267f4f71a06925017eccc65ff01ec96
Will-Copeland <will@localhost.localdomain>
2020-09-05 16:09:04 -0700

    Refactor js file
    - Extract fetchWeatherData, use try/catch
    - Extract updateDOM

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-window.addEventListener('load', ()=> {
-  let long;
-  let lat;
+window.addEventListener('load', async ()=> {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position =>{
+      const long = position.coords.longitude;
+      const lat = position.coords.latitude;
+
+      updateDOM(await fetchWeatherData(lat, long));
+    })
+  }
+    //const api2 = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  //function setIcons (icon, iconID){
+    //const skycons = new skycons({color: 'white'});
+    //const currentIcon = weatherIcon.replace(/-/g, "_")
+
+});
+
+
+function updateDOM(weatherData) {
   let temperatureDescription = document.querySelector('.temperature-description');
   let temperatureDegree = document.querySelector('.temperature-degree');
   let locationTimezone = document.querySelector('.location-timezone');
   let icon = document.querySelector('.icon');
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position =>{
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
+  const{description, weatherIcon} = data.weather[0];
+  const{temp_max} = data.main;
+  const{name} = data;
+  //set DOM elements from API
+  temperatureDegree.textContent = temp_max;
+  temperatureDescription.textContent = description;
+  locationTimezone.textContent = name;
+  // icon.backgroundImage = //Heres an edit
+}
 
-      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=dd088661d976220ba9d4d12cf3d1d70c`;
+async function fetchWeatherData(lat, long) {
+  const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=dd088661d976220ba9d4d12cf3d1d70c`;
 
-      //const pic = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-      fetch(api)
-      //fetch(pic)
-      .then(response =>{
-        return response.json();
-      })
-      .then(data =>{
-        console.log(data);
-        const{description, weatherIcon} = data.weather[0];
-        const{temp_max} = data.main;
-        const{name} = data;
-        //set DOM elements from API
-        temperatureDegree.textContent = temp_max;
-        temperatureDescription.textContent = description;
-        locationTimezone.textContent = name;
-        icon.backgroundImage = //Heres an edit
-      })
-    });
-    //const api2 = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-  //function setIcons (icon, iconID){
-    //const skycons = new skycons({color: 'white'});
-    //const currentIcon = weatherIcon.replace(/-/g, "_")
-  }
-});
+      try {
+        const data = await (await fetch(api)).json();
+        return data;
+      } catch (error) {
+        console.error(`Error fetching weather data: ${error}`);
+      }
+}
